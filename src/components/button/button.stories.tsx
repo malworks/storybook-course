63716b67d2fdb962c4540b0123088a4548cc6128
Mallory Robertson <malloryrobertson@Personal.local>
2024-11-16 16:01:13 -0800

    Added secondary and destructive buttons

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -7,12 +7,30 @@ type Story = StoryObj<typeof Button>;
 const meta: Meta<typeof Button> = {
 	title: 'Button',
 	component: Button,
-};
+    args: {
+		children: 'Button',
+	},
+} satisfies Meta;
 
 export default meta;
 
 export const Primary: Story = {
 	args: {
+		children: 'Button', // what is this actually for?
+		variant: 'primary',
+	},
+};
+
+export const Secondary: Story = {
+	args: {
 		children: 'Button',
+		variant: 'secondary',
+	},
+};
+
+export const Destructive: Story = {
+	args: {
+		children: 'Button',
+		variant: 'destructive',
 	},
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,15 @@
 import { ComponentProps } from 'react';
+import styles from './button.module.css';
 
-type ButtonProps = ComponentProps<'button'>;
-
-export const Button = (props: ButtonProps) => {
-	return <button {...props} />;
+export type ButtonProps = ComponentProps<'button'> & {
+	variant: 'primary' | 'secondary' | 'destructive';
 };
+
+export const Button = ({variant, ...props}: ButtonProps) => { // what does {variant, ...props} do?
+	let className = styles.button;
+	if (variant === 'secondary') className += ` ${styles.secondary}`;
+	if (variant === 'destructive') className += ` ${styles.destructive}`;
+
+	return <button className={className} {...props} />;
+};
+
