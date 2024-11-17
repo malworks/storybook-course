c9c1b97bb82129ec0ade31e1c2b1d23fc9aa179c
Mallory Iden <malloryworks@gmail.com>
2024-11-16 16:25:28 -0800

    Installed clsx

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,30 @@
 import { ComponentProps } from 'react';
 import styles from './button.module.css';
+import clsx from 'clsx';
 
 export type ButtonProps = ComponentProps<'button'> & {
 	variant: 'primary' | 'secondary' | 'destructive';
 };
 
-export const Button = ({variant, ...props}: ButtonProps) => { // what does {variant, ...props} do?
-	let className = styles.button;
-	if (variant === 'secondary') className += ` ${styles.secondary}`;
-	if (variant === 'destructive') className += ` ${styles.destructive}`;
+// export const Button = ({variant, className,...props}: ButtonProps) => { // what does {variant, ...props} do?
+// 	let classes = styles.button;
+// 	if (variant === 'secondary') className += ` ${styles.secondary}`;
+// 	if (variant === 'destructive') className += ` ${styles.destructive}`;
 
-	return <button className={className} {...props} />;
+// 	return <button className={classes} {...props} />;
+// 	// return <button {...props} className={classes} /> 
+// };
+
+export const Button = ({ variant = 'primary', ...props }: ButtonProps) => {
+	return (
+		<button
+			className={clsx(styles.button, {
+				[styles.secondary]: variant === 'secondary',
+				[styles.destructive]: variant === 'destructive',
+			})}
+			{...props}
+		/>
+	);
 };
+
 
